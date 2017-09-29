5898a3bc25af365b9f8d95cb5cb58c759879659f
Jeffrey Lowy <jeffreylowy@gmail.com>
2017-09-28 22:03:59 -0700

    added firebase, gitignore and deleted dsstore

---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const functions = require('firebase-functions');
+var request = require('request'); // "Request" library
+var client_id = 'f8148b6599904798a6df461ee469c1e8'; // Your client id
+var client_secret = 'cfb191024d79450a9733fe0bed8fd993'; // Your secret
+
+/*
+ *
+ * For more information, read
+ * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+ * https://firebase.google.com/docs/functions/write-firebase-functions
+ * 
+ */
+
+// your application requests authorization
+var authOptions = {
+	url: 'https://accounts.spotify.com/api/token',
+	headers: {
+		'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+	},
+	form: {
+		grant_type: 'client_credentials'
+	},
+	json: true
+};
+
+exports.helloWorld = functions.https.onRequest((req, res) => {
+	request.post(authOptions, function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			// use the access token to access the Spotify Web API
+			var token = body.access_token;
+			res.send(token);
+		}
+	});
+});
